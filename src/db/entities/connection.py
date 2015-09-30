21366ba3135a8178982177ed8011d2c0c1ca093f
marcelogaray <luis.marcelo.garay@gmail.com>
2015-09-30 17:04:56 -0400

    Revert "I'm submitting a initial classes for inventory, inventory manager, test_inventory"
    This reverts commit 9e8b62e4a501588be6d31e602467cfe4aadac5bd.

--- src/db/entities/connection.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'Ariel'
-import sqlite3
-from pathlib import Path
-
-# class to connect the database
-def connection():
-    database = sqlite3.connect("C:\Users\Ariel\Documents\SicariosRepo\src\db\entities")
-    return database
